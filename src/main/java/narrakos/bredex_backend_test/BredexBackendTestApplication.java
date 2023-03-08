4b1d575d5f47836eee93946fbd379d5bee8274f9
narrakos <narrakos96@gmail.com>
2023-03-08 15:52:46 +0100

    initial commit

---
+++ src/main/java/narrakos/bredex_backend_test/BredexBackendTestApplication.java
@@ -0,0 +1,13 @@
+package narrakos.bredex_backend_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BredexBackendTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BredexBackendTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/narrakos/bredex_backend_test/BredexBackendTestApplicationTests.java
@@ -0,0 +1,13 @@
+package narrakos.bredex_backend_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BredexBackendTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
